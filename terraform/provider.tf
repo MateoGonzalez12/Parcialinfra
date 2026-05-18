43de44a55e950edfa85a67a3256a21bbce4e310b
MateoGonzalez12 <ivmateo777@gmail.com>
2026-05-18 09:58:20 -0500

    eliminación s3

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,16 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
-  }
-
-  backend "s3" {
-    bucket = "flask-app-terraform-state"
-    key    = "flask-app/terraform.tfstate"
-    region = "us-east-1"
   }
 }
 
